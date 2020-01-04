b5a53abc0b098c01ba867ec5e3eca06d69fdaa47
Cleber Miranda Barbosa <cleber@gmail.com>
2020-01-04 13:11:16 -0300

    images of each character

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../../dist/img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import "../component/titlebar/TitleBar";
 import TitleBar from "../component/titlebar/TitleBar";
@@ -12,20 +12,3 @@ const Home = () => (
 );
 
 export default Home;
-
-/*
-
-const Home = () => (
-	<div className="text-center mt-5">
-		<TitleBar title="STAR WARS: THE RISE OF SKYWALKER //"/>
-		<h1>Hello Rigo!</h1>
-
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
-*/
